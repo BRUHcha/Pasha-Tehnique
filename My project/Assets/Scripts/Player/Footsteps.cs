bb5f604f0b5bee04ec76f6363e6b354f8431159e
BRUHcha <seruy31.01@mail.ru>
2024-08-19 14:59:56 +0300

    new footsteps sounds

--- My project/Assets/Scripts/Player/Footsteps.cs
+++ My project/Assets/Scripts/Player/Footsteps.cs
@@ -11,6 +11,7 @@ public class Footsteps : Sounds
 
     private AudioSource steps;
     private Ray GroundDetector;
+    private string previousTag = null;
 
     private void Start()
     {
@@ -18,16 +19,20 @@ public class Footsteps : Sounds
     }
     private void Update()
     {
-        if((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)) && characterController.isGrounded)
-        {
-            RaycastHit hit;
-            GroundDetector = new Ray(transform.position, transform.forward * rayLen);
+        GroundDetector = new Ray(transform.position, transform.forward * rayLen);
 
-            if (Physics.Raycast(GroundDetector, out hit, rayLen, ground))
-            {
-                if (hit.collider.tag == "Carpet") steps.clip = clips[0];
-                if (hit.collider.tag == "Wood") steps.clip = clips[1];
-            }
+        if (Physics.Raycast(GroundDetector, out RaycastHit hit, rayLen, ground))
+        {
+            if (hit.collider.CompareTag("Carpet")) steps.clip = clips[0];
+            if (hit.collider.CompareTag("Wood")) steps.clip = clips[1];
+            if (hit.collider.CompareTag("Stone")) steps.clip = clips[2];
+            else
+                Debug.Log(hit.collider.gameObject.name);
+        }
+        if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)) && characterController.isGrounded)
+        {
+            if(!hit.collider.CompareTag(previousTag))
+                steps.enabled = false;
 
             steps.enabled = true;
         }
@@ -35,5 +40,6 @@ public class Footsteps : Sounds
         {
             steps.enabled = false;
         }
+        previousTag = hit.collider.tag;
     }
 }
